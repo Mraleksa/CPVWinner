4300da6cdef8aaa333e065c256a5166094d06d66
Mraleksa <a.gribanovskiy@gmail.com>
2017-08-03 18:21:52 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -10,24 +10,26 @@ var p=0; var p2=0;
  
 function piv(){  
 p++;
-client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?offset='+currentCount})
+client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders?offset='+currentCount})
 		.then(function (data) {
 			var dataset = data.getJSON().data;			
 			currentCount = data.getJSON().next_page.offset;			
 			console.log(currentCount)			
 			return dataset;
 		})	
+
 		.then(function (dataset) {			
 			dataset.forEach(function(item) {
-				client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts/'+item.id})
+				client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+item.id})
 					.then(function (data) {
 					
-var description = data.getJSON().data.items[0].description.toLowerCase();					
+					
 db.serialize(function() {
-db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,description TEXT,cpv TEXT,winner TEXT,winnerEdr TEXT,value INT,contactPoint TEXT,contractID TEXT)");
-var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?)");
-statement.run(item.dateModified,description,data.getJSON().data.items[0].classification.id,
-data.getJSON().data.suppliers[0].name,data.getJSON().data.suppliers[0].identifier.id,data.getJSON().data.value.amount,data.getJSON().data.suppliers[0].contactPoint.email,data.getJSON().data.contractID);
+db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,procurementMethod TEXT)");
+var statement = db.prepare("INSERT INTO data VALUES (?,?)");
+
+statement.run(item.dateModified,data.getJSON().procurementMethod);
+
 statement.finalize();
 });
 			
@@ -39,8 +41,9 @@ statement.finalize();
 				});
 		
 		})
+		*/
 		.then(function () {	
-		if (p<10){piv ();}		
+		if (p<3){piv ();}		
 		else {
 			console.log("stop")
 				p=0;
@@ -48,7 +51,7 @@ statement.finalize();
 				console.log(p2)
 			setTimeout(function() {
 			
-				if (p2 < 100) {
+				if (p2 < 2) {
 					piv ();
 				}
 				else {console.log("STOP")}
